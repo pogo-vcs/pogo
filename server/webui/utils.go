ea4aac6b1273b2ca47309e3ca58c163858a1182c
Frank Mayer <gh@frankmayer.dev>
2025-10-10 18:16:30 +0200

    feat(ui): file tree for repo web ui

--- server/webui/utils.go
+++ server/webui/utils.go
@@ -3,7 +3,9 @@ package webui
 import (
 	"context"
 	"net/http"
+	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/pogo-vcs/pogo/auth"
@@ -103,4 +105,68 @@ func IsLoggedIn(ctx context.Context) bool {
 		return user != nil
 	}
 	return false
+}
+
+type FileNode struct {
+	Name     string
+	FullPath string
+	IsDir    bool
+	File     *db.GetRepositoryFilesRow
+	Children []*FileNode
+}
+
+func BuildFileTree(files []db.GetRepositoryFilesRow) *FileNode {
+	root := &FileNode{IsDir: true, Children: []*FileNode{}}
+
+	for _, file := range files {
+		parts := strings.Split(file.Name, "/")
+		current := root
+
+		for i, part := range parts {
+			isLastPart := i == len(parts)-1
+
+			var child *FileNode
+			for _, c := range current.Children {
+				if c.Name == part {
+					child = c
+					break
+				}
+			}
+
+			if child == nil {
+				child = &FileNode{
+					Name:     part,
+					FullPath: strings.Join(parts[:i+1], "/"),
+					IsDir:    !isLastPart,
+					Children: []*FileNode{},
+				}
+				if isLastPart {
+					child.File = &file
+				}
+				current.Children = append(current.Children, child)
+			}
+
+			current = child
+		}
+	}
+
+	sortFileNodes(root)
+	return root
+}
+
+func sortFileNodes(node *FileNode) {
+	if !node.IsDir {
+		return
+	}
+
+	sort.Slice(node.Children, func(i, j int) bool {
+		if node.Children[i].IsDir != node.Children[j].IsDir {
+			return node.Children[i].IsDir
+		}
+		return node.Children[i].Name < node.Children[j].Name
+	})
+
+	for _, child := range node.Children {
+		sortFileNodes(child)
+	}
 }
